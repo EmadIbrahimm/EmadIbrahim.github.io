44640c0b8893e5d43340d417a3bcb7f73f42450e
Emad Ibrahim <emad.ibrahim533@gmail.com>
2020-11-27 18:49:49 +0100

    modification

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const body = document.body;
 const btn = document.querySelectorAll('.button')[0];
+const headingOne = document.querySelector('.heading1')
+const headingTwo = document.querySelector('.my-box')
+
+
+const tl = new TimelineMax();
+
+tl.fromTo(headingOne, 1,{height:'0%'}, {height: '80%', ease: Power2.easeInOut })
+.fromTo(headingTwo, 0.5, {opacity: 0, x:30}, {opacity:1, x:0}, "0.5")
+
 
 btn.addEventListener('mouseenter', () => {
 	body.classList.add('show');
@@ -8,3 +17,4 @@ btn.addEventListener('mouseenter', () => {
 btn.addEventListener('mouseleave', () => {
 	body.classList.remove('show');
 });
+
