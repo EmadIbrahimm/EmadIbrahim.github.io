f9ff0ae98253787b71b226fdb20e8c331fe69d7b
Emad Ibrahim <emad.ibrahim533@gmail.com>
2020-11-28 18:21:20 +0100

    commit on master

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const body = document.body;
 const btn = document.querySelectorAll('.button')[0];
-const headingOne = document.querySelector('.heading1')
+const headingOne = document.querySelector('.navbar-brand')
 const headingTwo = document.querySelector('.my-box')
 
 
-const tl = new TimelineMax();
 
-tl.fromTo(headingOne, 1,{height:'0%'}, {height: '80%', ease: Power2.easeInOut })
-.fromTo(headingTwo, 0.5, {opacity: 0, x:30}, {opacity:1, x:0}, "0.5")
+var tl = new TimelineMax({});
+	tl.to("body", .3, {backgroundColor:"blue"});
+
+// const tl = new TimelineMax();
+
+// tl.fromTo(headingOne, 1,{height:'0%'}, {height: '80%', ease: Power2.easeInOut })
+// .fromTo(headingTwo, 0.5, {opacity: 0, x:30}, {opacity:1, x:0}, "1")
 
 
-btn.addEventListener('mouseenter', () => {
-	body.classList.add('show');
-});
+// btn.addEventListener('mouseenter', () => {
+// 	body.classList.add('show');
+// });
 
-btn.addEventListener('mouseleave', () => {
-	body.classList.remove('show');
-});
+// btn.addEventListener('mouseleave', () => {
+// 	body.classList.remove('show');
+// });
 
